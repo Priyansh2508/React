7db134788199f89391b97ae4afff009331a1c6fb
priyansh agarwal <priyanshagarwal@priyanshs-MacBook-Air.local>
2025-03-30 15:31:53 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React 
+
+# Parcel
+-Dev Build
+-local server
+-HMR Hot module replacement
+-file watching algorithm in c++
+-caching faster builds
+-image optimization
+-minification
+-bundling
+-compressing
+-cinsistent hashing
+-code splitting
+-differential bundling
+-Diagnostic
+-Error Handling
+-Tree shaking- remove extra code
+-HTTPs
+-different dev and prod bundles
--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id: "parent"},
-    React.createElement(
-        "div",
-        {id: "child"},[
-        React.createElement("h1",{key:1},"Namaste rea"),
-        React.createElement("h2",{key:2},"second tag")
-    ])
-    
-);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+//React Element
+
+const heading = React.createElement("h1",{id: "heading"},"Namaste React 🤓");
+
+console.log(heading);
+
+// JSX (transpiled before it reaches the JS) - Parcel - Babel
+
+//JSX => babel transpiles it to React.createElement => reactElement object => render does HTMLElement
+
+//React element
+const jsxheading = (<h1 className="head">
+    Namaste React using JSX by priyansh
+    </h1>);
+
+//React componenets
+//clss based components OLD
+//functional components NEW
+
+//react functional component function that returns jsx code
+const HeadingComponent=()=>{
+    return <h1>Namaste React functional components</h1>
+}
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
